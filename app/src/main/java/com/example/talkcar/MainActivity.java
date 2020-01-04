796857a124d5947a9b19a6c513782bf46cfae729
Sarel <sarelmicha93@gmail.com>
2020-01-04 15:54:40 +0200

    *Camera in process

--- app/src/main/java/com/example/talkcar/MainActivity.java
+++ app/src/main/java/com/example/talkcar/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.talkcar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView takePhoto;
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,18 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
     private void setIds(){
 
+        takePhoto = (ImageView)findViewById(R.id.take_photo);
+    }
+
+    private void setClickListeners(){
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     @Override
@@ -41,5 +64,21 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         popup.setOnMenuItemClickListener(this);
         popup.inflate(R.menu.popup_menu);
         popup.show();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
