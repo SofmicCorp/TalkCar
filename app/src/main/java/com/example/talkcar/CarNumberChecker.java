98caae62828308bc3c07f391fabce1726dd47912
Sarel <sarelmicha93@gmail.com>
2020-01-05 19:09:21 +0200

    *Just xml added on sign up on master sorry im an idiot

--- app/src/main/java/com/example/talkcar/CarNumberChecker.java
+++ app/src/main/java/com/example/talkcar/CarNumberChecker.java
@@ -1,16 +1,5 @@
 package com.example.talkcar;
 
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 public class CarNumberChecker {
 
     private final int ZERO = 48;
