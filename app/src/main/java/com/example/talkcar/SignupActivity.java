ae3a4eb67e255068f9e7c4ee92f2a05f070c12ca
Sarel <sarelmicha93@gmail.com>
2020-01-08 12:52:14 +0200

    Delete a form has completed perfectly!!

--- app/src/main/java/com/example/talkcar/SignupActivity.java
+++ app/src/main/java/com/example/talkcar/SignupActivity.java
@@ -22,7 +22,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -30,15 +29,11 @@ public class SignupActivity extends AppCompatActivity {
     private EditText passwordPlaceHolder;
     private Button signInBtn;
     private ImageView addCar;
-    private int numOfCars = 0;
-    private InputManager inputManager;
     private Database databaseRef;
-    private HashMap<ImageView,Integer> indexes; // for saving the index of every emoji extra car form
     private FirebaseAuth mFirebaseAuth;
     private Driver driver;
-    private ArrayList<TextView> allFormHeadersTextViews;
-    private DynamicallyXML dynamicallyXML;
     private LinearLayout allFormContainer;
+    private DynamicallyXML dynamicallyXML;
 
 
     @Override
@@ -46,11 +41,8 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         setIds();
-        inputManager = new InputManager(); //Manage all cars that a driver have when register
         mFirebaseAuth = FirebaseAuth.getInstance();
-        indexes = new HashMap<>();
         databaseRef = new Database();
-        allFormHeadersTextViews = new ArrayList<>();
         dynamicallyXML = new DynamicallyXML();
         setClickListeners();
         createAddCarForm();
@@ -69,6 +61,7 @@ public class SignupActivity extends AppCompatActivity {
         addCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 createAddCarForm();
             }
         });
@@ -76,164 +69,8 @@ public class SignupActivity extends AppCompatActivity {
 
     private void createAddCarForm() {
 
-        //Create Form layouts
-        LinearLayout formHeaderAndDeleteContainer = new LinearLayout(this);
-        LinearLayout inputUserContainer = new LinearLayout(this);
-        LinearLayout emojiContainer = new LinearLayout(this);
-
-        //Set Layouts Orientation
-        inputUserContainer.setOrientation(LinearLayout.VERTICAL);
-        formHeaderAndDeleteContainer.setOrientation(LinearLayout.HORIZONTAL);
-        emojiContainer.setOrientation(LinearLayout.HORIZONTAL);
-
-
-        TextView carNumbernth = dynamicallyXML.createTextView(this,"Car number #" + (numOfCars + 1),18,Color.rgb(44,167,239),Gravity.CENTER,100,50,0,0);
-        ImageView deleteFormBtn = dynamicallyXML.createImageView(this,R.drawable.minussign,70,70,Gravity.CENTER,-200,25,0,0);
-        allFormHeadersTextViews.add(carNumbernth);
-        addAllViewsLayout(formHeaderAndDeleteContainer,carNumbernth,deleteFormBtn);
-
-        EditText carNumberPlaceHolder =  dynamicallyXML.createEditText(this,"Car Number",InputType.TYPE_CLASS_PHONE);
-        inputManager.getAllCarNumbers().add(carNumberPlaceHolder);
-        EditText nicknamePlaceHolder = dynamicallyXML.createEditText(this,"Nickname",InputType.TYPE_CLASS_TEXT);
-        inputManager.getAllNickNames().add(nicknamePlaceHolder);
-        TextView pickYourEmojiText = dynamicallyXML.createTextView(this,"Pick your emoji's car!",13,Color.BLACK,Gravity.CENTER,220,50,0,0);
-        addAllViewsLayout(inputUserContainer,carNumberPlaceHolder,nicknamePlaceHolder,pickYourEmojiText);
-
-        //add To Emoji Container
-        addEmojiToContainer(emojiContainer);
-        addAllViewsLayout(allFormContainer,formHeaderAndDeleteContainer,inputUserContainer,emojiContainer);
-
-        indexes.put(deleteFormBtn,numOfCars);
-        numOfCars++;
-
-
-        setFormListeners(deleteFormBtn,formHeaderAndDeleteContainer,inputUserContainer,emojiContainer);
+        NewCarForm newCarForm = new NewCarForm(this,allFormContainer);
     }
-
-    private void setFormListeners(final ImageView deleteFormBtn, final LinearLayout formHeaderAndDeleteContainer, final LinearLayout inputUserContainer, final LinearLayout emojiContainer ) {
-
-        deleteFormBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteForm(formHeaderAndDeleteContainer, inputUserContainer,emojiContainer);
-            }
-        });
-    }
-
-    private void deleteForm(LinearLayout formHeaderAndDeleteContainer, LinearLayout inputUserContainer, LinearLayout emojiContainer) {
-
-        formHeaderAndDeleteContainer.removeAllViews();
-        inputUserContainer.removeAllViews();
-        emojiContainer.removeAllViews();
-
-
-        allFormHeadersTextViews.remove(numOfCars);
-
-        formHeaderAndDeleteContainer = null;
-        inputUserContainer = null;
-        emojiContainer = null;
-
-        numOfCars--;
-
-        updateAllFormHeadersTextViews();
-    }
-
-    private void updateAllFormHeadersTextViews() {
-
-        for(int i = 0; i < allFormHeadersTextViews.size(); i++){
-            allFormHeadersTextViews.get(i).setText("Car nuumberrr #" + (i + 1));
-        }
-    }
-
-    private void addAllViewsLayout(LinearLayout layout,View... view) {
-
-        for(View v: view){
-            layout.addView(v);
-        }
-    }
-
-    private void addEmojiToContainer(LinearLayout emojiContainer) {
-
-        ImageView driverOne = dynamicallyXML.createImageView(this,R.drawable.driver1,200,200,Gravity.CENTER,100,20,0,0);
-        ImageView driverTwo = dynamicallyXML.createImageView(this,R.drawable.driver2,200,200, Gravity.CENTER,100,20,0,0);
-        ImageView driverThree = dynamicallyXML.createImageView(this,R.drawable.driver3,200,200,Gravity.CENTER,100,20,0,0);
-
-        indexes.put(driverOne,numOfCars); // when we add more car form we add car and index to the indexes hashmap
-                                            //driverOne is just a mark to know which form are we. it doesnt matter at all if
-                                            //it will be driverOne or driverTwo or driverThree, it is just a mark for hashmap
-
-        setEmojiClickListeners(driverOne,driverTwo,driverThree);
-        addAllViewsLayout(emojiContainer,driverOne, driverTwo, driverThree);
-
-
-    }
-
-    private void setEmojiClickListeners(final ImageView driverOne, final ImageView driverTwo, final ImageView driverThree) {
-
-        driverOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                markEmoji(driverOne,Color.rgb(44,167,239));
-                markEmoji(driverTwo, Color.WHITE);
-                markEmoji(driverThree, Color.WHITE);
-
-                try{
-                    driverOne.setTag(1);
-                    inputManager.getAllEmojisIds().set(indexes.get((driverOne)),driverOne.getTag().toString());
-                } catch(IndexOutOfBoundsException e){
-                    //if you are here it means its a new form
-                    inputManager.getAllEmojisIds().add(driverOne.getTag().toString());
-                }
-
-            }
-        });
-
-        driverTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                markEmoji(driverOne, Color.WHITE);
-                markEmoji(driverTwo,Color.rgb(44,167,239));
-                markEmoji(driverThree, Color.WHITE);
-
-                try{
-                    driverTwo.setTag(2);
-                    inputManager.getAllEmojisIds().set(indexes.get((driverOne)),driverTwo.getTag().toString());
-                } catch(IndexOutOfBoundsException e){
-                    inputManager.getAllEmojisIds().add(driverTwo.getTag().toString());
-                }
-            }
-        });
-
-        driverThree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                markEmoji(driverOne, Color.WHITE);
-                markEmoji(driverTwo, Color.WHITE);
-                markEmoji(driverThree,Color.rgb(44,167,239));
-
-
-                try{
-                    driverThree.setTag(3);
-                    inputManager.getAllEmojisIds().set(indexes.get((driverOne)),driverThree.getTag().toString());
-                } catch(IndexOutOfBoundsException e){
-
-                    inputManager.getAllEmojisIds().add(driverThree.getTag().toString());
-                }
-            }
-        });
-    }
-
-    private void markEmoji(ImageView emoji,int color) {
-
-        GradientDrawable gd = new GradientDrawable();
-        gd.setColor(Color.WHITE);
-        gd.setCornerRadius(100);
-        gd.setStroke(4, color);
-        emoji.setBackgroundDrawable(gd);
-    }
-
 
     private void createFireBaseUser(){
         final String email = emailPlaceHolder.getText().toString().trim();
@@ -267,10 +104,10 @@ public class SignupActivity extends AppCompatActivity {
     private void saveDriverToDatabase(String email){
 
         driver = new Driver(email);
-        for(int i = 0; i < numOfCars; i++){
-            String carNumber = inputManager.getAllCarNumbers().get(i).getText().toString();
-            String nickName = inputManager.getAllNickNames().get(i).getText().toString();
-            String emojiId = inputManager.getAllEmojisIds().get(i);
+        for(int i = 0; i < NewCarForm.allForms.size(); i++){
+            String carNumber = NewCarForm.allForms.get(i).getCarNumberPlaceHolder().getText().toString();
+            String nickName = NewCarForm.allForms.get(i).getNicknamePlaceHolder().toString();
+            String emojiId = NewCarForm.allForms.get(i).getEmojiID();
             driver.addCar(new Car(carNumber, nickName,emojiId));
         }
         databaseRef.saveDriver(driver);
@@ -286,7 +123,5 @@ public class SignupActivity extends AppCompatActivity {
         passwordPlaceHolder = (EditText)findViewById(R.id.password_placeholder);
         signInBtn = (Button)findViewById(R.id.signin_btn);
         addCar = (ImageView)findViewById(R.id.plus_sign);
-
     }
-
 }
