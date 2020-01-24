a69ab02fcd89985f37279f9bcef0d3034b40300b
Sarel <sarelmicha93@gmail.com>
2020-01-24 14:31:55 +0200

    fix bugs

--- app/src/main/java/com/example/talkcar/MainActivity.java
+++ app/src/main/java/com/example/talkcar/MainActivity.java
@@ -335,6 +335,7 @@ public class MainActivity extends AppCompatActivity implements OnInputListener {
         databaseRef.saveDriver(driver);
 
         updateCarView(driver.getCars().get(carView.getCardId()),carView.getCardId());
+        updateCarPickerIcon(carView.getCardId());
 
     }
 
